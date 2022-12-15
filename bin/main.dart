eb008fb39716603654b2d69d513b5a6a55908fd8
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:26:28 +0200

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,29 @@
+// import packages and classes
 
-import 'Employee.dart';
 import 'TeamBE.dart';
+import 'TeamMarketing.dart';
+import 'TeamUI.dart';
 
 void main(List<String> arguments) {
+  // create object from the class BackEndTeam
+  var beObj = BackEndTeam();
 
+// calling fun from the class BackEndTeam
+  beObj.engSalaryPerYear();
+  print("-----------------"); // separate between result of classes
+//##############################################################################
 
-var beObj = BackEndTeam();
-beObj.engSalaryPerYear();
+  // create object from the class UITeam
+  var uiObj = UITeam();
 
+// calling fun from the class UITeam
+  uiObj.engSalaryPerYear();
+  print("-----------------"); // separate between result of classes
+//##############################################################################
 
+  // create object from the class MarketingTeam
+  var mObj = MarketingTeam();
+
+// calling fun from the class MarketingTeam
+  mObj.empSalaryPerYear();
 }
