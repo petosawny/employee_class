a2267c21333bf8993ac4ea24d2dce81d5d0a6a5a
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:27:00 +0200

    Update TeamUI.dart

--- bin/TeamUI.dart
+++ bin/TeamUI.dart
@@ -1,12 +1,16 @@
+// import packages and classes
 
 import 'Employee.dart';
 
 class UITeam extends Employee {
+  //variable
+
   num? salary= 7000;
 
+  // num Function
 
   num? engSalaryPerYear() {
-    getInfo();
+    getInfo();        // calling Function from Master Class
 
     num? engSalary = salary! * 12 * experienceYears! ;
     return engSalary;
