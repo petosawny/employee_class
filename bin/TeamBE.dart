3599fd8e89d1180a30b97ffd4c98fd3e25d21b28
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:28:35 +0200

    Update TeamBE.dart

--- bin/TeamBE.dart
+++ bin/TeamBE.dart
@@ -1,16 +1,20 @@
+// import packages and classes
 
 import 'dart:io';
 
 import 'Employee.dart';
 
 class BackEndTeam extends Employee {
+  //variable with initialize value
 
   num salary = 7000;
 
+  // num Function
   num? engSalaryPerYear() {
-    getInfo();
-    num? engSalary = salary * 12 * experienceYears! ;
-    print(   name! + "your salary per year is equal " + engSalary.toString()  );
+    getInfo();         // calling Function from Master Class
+
+    num? engSalary = salary * 12 * experienceYears!;
+    print(name! + "your salary per year is equal " + engSalary.toString());
     return engSalary;
   }
 }
