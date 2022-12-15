174c2b409dbb12a3633e2825eeb008352e9b9bd9
petosawny <93099971+petosawny@users.noreply.github.com>
2022-12-15 03:28:11 +0200

    Update TeamMarketing.dart

--- bin/TeamMarketing.dart
+++ bin/TeamMarketing.dart
@@ -1,14 +1,17 @@
+// import packages and classes
 
 import 'Employee.dart';
 
 class MarketingTeam extends Employee {
   num? salary;
 
+  // num Function
 
   num? empSalaryPerYear() {
-    getInfo();
+    getInfo();        // calling Function from Master Class
 
-    num? engSalary = salary! * 12 * experienceYears! ;
+
+    num? engSalary = salary! * 12 * experienceYears! ;  //define variavle and give it eguation
     return engSalary;
   }
 }
